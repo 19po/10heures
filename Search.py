1628f14b5f719a747c1de345c17a4ba330b916af
post <postrowski.89@wp.pl>
2016-01-27 23:45:32 +0100

    Enhanced searching and string handling.
    * Enhanced searching.
    * Fixes unicode string handling.

--- Search.py
+++ Search.py
@@ -7,15 +7,14 @@ from PyQt4 import QtCore
 import urllib
 import json
 from collections import OrderedDict
-
+import re
 
 class Search(object):
     def __init__(self, parent):
 
-        self.art_lists_1u = []
-        self.alb_lists_1u = []
-        self.alb_lists_old_1u = []
-        self.alb_lists_new_2u = []
+        self.art_dict_u = {}
+        self.alb_dict = {}
+        self.alb_track_dict = {}
 
         self.artList = parent.artList
         self.albList = parent.albList
@@ -26,46 +25,38 @@ class Search(object):
 
     # -----------find-artist-names------------
 
-    def find_art(self):
-
-        # information to user: list is not ready to play album
-        self.albList.setStyleSheet("QListWidget {background-color: #FFFFFF}")
-
-        get_search_edit = QtCore.QString(unicode(self.searchEdit.text()))
-        print(u"Search for: {0}".format(unicode(get_search_edit)))
+    def find_artist(self):
+        """
+        Find artists
+        """
 
         self.artList.clear()
         self.albList.clear()
         self.sngList.clear()
 
+        # information to user: list is not ready to play album
+        self.albList.setStyleSheet("QListWidget {background-color: None}")
+
+        # get input
+        get_search_edit = QtCore.QString(unicode(self.searchEdit.text()))
         search_url = u'https://api.deezer.com/search?q={0}'.format(unicode(get_search_edit))
         print search_url
 
+        # console information
+        print(u"Search for: {0}".format(unicode(get_search_edit)))
+
+        # open json file
         json_url = urllib.urlopen(search_url)
         data = json.load(json_url)
-
-        # -------------get-values-from-json-file----------------
-
-        art_id_list = []
-        art_name_list = []
-        # art_picture_small_list = []
-        art_tracklist_list = []
-
-        alb_id_list = []
-        alb_title_list = []
-        # alb_cover_small_list = []
-        alb_tracklist_list = []
-
-        sng_id_list = []
-        sng_title_list = []
 
         # check index
         li = (len(data["data"]))
 
-        for i in range(li):
-            art_id = data["data"][i]["artist"]["id"]
-            art_id_list.append(art_id)
+        # get values from json file
+        art_name_list, art_tracklist_list = [], []  # art_picture_small_list = []
+        alb_title_list, alb_tracklist_list = [], []  # alb_cover_small_list = []
 
+        for i in range(li):
             art_name = data["data"][i]["artist"]["name"]
             art_name_list.append(art_name)
 
@@ -74,9 +65,6 @@ class Search(object):
 
             art_tracklist = data["data"][i]["artist"]["tracklist"]
             art_tracklist_list.append(art_tracklist)
-
-            alb_id = data["data"][i]["album"]["id"]
-            alb_id_list.append(alb_id)
 
             alb_title = data["data"][i]["album"]["title"]
             alb_title_list.append(alb_title)
@@ -87,78 +75,57 @@ class Search(object):
             alb_tracklist = data["data"][i]["album"]["tracklist"]
             alb_tracklist_list.append(alb_tracklist)
 
-            sng_id = data["data"][i]["id"]
-            sng_id_list.append(sng_id)
+        # make dictionary {"artist name": ["artist tracklist", ...], ...}
+        art_dict = {i: [] for i in set(art_name_list)}
+        [art_dict[x].append(y) for (x, y) in zip(art_name_list, art_tracklist_list)]
+        # ...and remove duplicates from values
+        self.art_dict_u = {i: j for (i, j) in zip(art_dict.keys(), [unique(art_dict.values()[i]) for i in
+                                                                    range(len(art_dict.values()))])}
 
-            sng_title = data["data"][i]["title"]
-            sng_title_list.append(sng_title)
+        # make {"art name":[{"album name" : "album tracklist", ...}], ...}
+        out_dict = {i: [] for i in set(art_name_list)}
+        [out_dict[x].append(y) for (x, y) in zip(art_name_list, zip(alb_title_list, alb_tracklist_list))]
+        # ...make inner dictionary...
+        in_dict = [dict(out_dict.values()[i]) for i in range(len(out_dict))]
+        # ...join outer and inner dictionary
+        self.alb_dict = {i: [j] for (i, j) in zip(out_dict.keys(), in_dict)}
 
-        # ---------remove-duplicates-from-lists---------------
+        [self.artList.addItem(self.alb_dict.keys()[i]) for i in range(len(self.alb_dict))]
 
-        art_lists_1 = [art_id_list] + [art_name_list] + [art_tracklist_list]
-        i1 = len(art_lists_1) - 1
-        self.art_lists_1u = [unique(art_lists_1[i1]) for art_lists_1[i1] in art_lists_1[:]]
-
-        alb_lists_1 = [alb_id_list] + [alb_title_list]
-        i2 = len(alb_lists_1) - 1
-        self.alb_lists_1u = [unique(alb_lists_1[i2]) for alb_lists_1[i2] in alb_lists_1[:]]
-
-        sng_lists_1 = [sng_id_list] + [sng_title_list]
-        i3 = len(sng_lists_1) - 1
-        sng_lists_1u = [unique(sng_lists_1[i3]) for sng_lists_1[i3] in sng_lists_1[:]]
-
-        # -----------old-album-titles-to-previous-album-search----------
-
-        alb_lists_old_1 = [alb_id_list] + [alb_title_list] + [alb_tracklist_list]
-        old = len(alb_lists_old_1) - 1
-        self.alb_lists_old_1u = [unique(alb_lists_old_1[old]) for alb_lists_old_1[old] in alb_lists_old_1[:]]
-
-        # --------------add-elements-to-lists-----------------
-
-        [self.artList.addItem(i) for i in self.art_lists_1u[1]]
-        [self.albList.addItem(i) for i in self.alb_lists_1u[1]]
-        [self.sngList.addItem(i) for i in sng_lists_1u[1]]
-
-        return self.art_lists_1u, self.alb_lists_1u, self.alb_lists_old_1u  # , self.new_nd_name_2, self.new_nd_name
+        return self.art_dict_u, self.alb_dict
 
     # ------------find-album-titles-------------
 
-    def find_alb(self):
-
-        # information to user: list is ready to play album
-        self.albList.setStyleSheet("QListWidget {background-color: rgba(10, 20, 128, 30%)}")
-
-        get_artist = QtCore.QString(unicode(self.artList.currentRow()))
-        get_artist_name = QtCore.QString(unicode(self.artList.currentItem().text()))
-
-        get_art_tracklist = self.art_lists_1u[2][int(get_artist)]
-
-        print(u"Search for {0} albums.".format(unicode(get_artist_name)))
-        print(get_art_tracklist)
+    def find_album(self):
+        """
+        Find artist albums.
+        """
 
         self.albList.clear()
         self.sngList.clear()
 
-        json_url = urllib.urlopen(get_art_tracklist)
+        # information to user: list is ready to play album
+        self.albList.setStyleSheet("QListWidget {background-color: rgba(10, 20, 128, 30%)}")
+
+        # get checked album title
+        get_artist_name = QtCore.QString(unicode(self.artList.currentItem().text()))
+        # ...and get his tracklist
+        get_art_tracklist = self.art_dict_u[unicode(get_artist_name)][0]
+
+        # console information
+        print(u"Search for {0} albums.".format(unicode(get_artist_name)))
+        print(''.join(get_art_tracklist))  # list to string
+
+        # open json file
+        json_url = urllib.urlopen(''.join(get_art_tracklist))  # list to string
         data = json.load(json_url)
-
-        # -------------get-values-from-json-file----------------
-
-        alb_id_list = []
-        alb_title_list = []
-        alb_tracklist_list = []
-        # alb_cover_big_list = []
-
-        sng_id_list = []
-        sng_title_list = []
 
         # check index
         li = (len(data["data"]))
 
+        # get values from json file
+        alb_title_list, alb_tracklist_list = [], []  # alb_cover_big_list = []
         for i in range(li):
-            alb_id = data["data"][i]["album"]["id"]
-            alb_id_list.append(alb_id)
-
             alb_title = data["data"][i]["album"]["title"]
             alb_title_list.append(alb_title)
 
@@ -168,108 +135,83 @@ class Search(object):
             # alb_cover_big = data['data'][i]['album']['cover_big']
             # alb_cover_big_list.append(alb_cover_big)
 
-            sng_id = data["data"][i]["id"]
-            sng_id_list.append(sng_id)
+        # get chosen artist albums from previous search
+        prev_album = self.alb_dict[str(get_artist_name)][0].keys()
+        # ...merge and remove duplicates previous and current albums
+        album = []
+        for i in range(len([prev_album + alb_title_list])):
+            album = unique([prev_album + alb_title_list][i])
 
-            sng_title = data["data"][i]["title"]
-            sng_title_list.append(sng_title)
+        # get chosen artist album tracklist from previous search
+        prev_alb_track = self.alb_dict[str(get_artist_name)][0].values()
+        # ...merge and remove duplicates previous and current albums
+        album_track = ''
+        for i in range(len([prev_alb_track + alb_tracklist_list])):
+            album_track = unique([prev_alb_track + alb_tracklist_list][i])
 
-        # ---------remove-duplicates-from-lists---------------
+        # paste albums to list
+        [self.albList.addItem(album[i]) for i in range(len(album))]
 
-        alb_lists_2 = [alb_id_list] + [alb_title_list] + [alb_tracklist_list]
-        i2 = len(alb_lists_2) - 1
-        self.alb_lists_2u = [unique(alb_lists_2[i2]) for alb_lists_2[i2] in alb_lists_2[:]]
+        # make dictionary {"album title" : "album tracklist"}
+        self.alb_track_dict = {i: j for (i, j) in zip(album, album_track)}
 
-        sng_lists_2 = [sng_id_list] + [sng_title_list]
-        i3 = len(sng_lists_2) - 1
-        sng_lists_2u = [unique(sng_lists_2[i3]) for sng_lists_2[i3] in sng_lists_2[:]]
+        return self.alb_track_dict
 
-        # join album titles from artist search and this search; delete duplicates
-
-        alb_lists_new_2 = [self.alb_lists_2u[i] + self.alb_lists_old_1u[i] for i in range(0,3)]
-        i4 = len(alb_lists_new_2) - 1
-        self.alb_lists_new_2u = [unique(alb_lists_new_2[i4]) for alb_lists_new_2[i4] in alb_lists_new_2[:]]
-
-        # --------------add-elements-to-lists-----------------
-
-        [self.albList.addItem(i) for i in self.alb_lists_new_2u[1]]
-        [self.sngList.addItem(i) for i in sng_lists_2u[1]]
-
-        return self.alb_lists_new_2u
-
-    # -------------find-song-titles--------------
-
-    def find_sng(self):
-
+    def find_song(self):
         """
         Find album songs.
-        Method check active current row in List Widget (albList), get his index (get_alb) and his name (get_alb_name).
-        Then using index (get_alb) get his website link (get_alb_tracklist) from list (alb_lists_2u).
-        Clear List Widget (sngList) for new data. Open website link with json (data) file. Make list of songs titles
-        (sng_title_list) and paste it to List Widget (sngList).
         """
-        get_alb = QtCore.QString(unicode(self.albList.currentRow()))
-        get_alb_name = QtCore.QString(unicode(self.albList.currentItem().text()))
-
-        get_alb_tracklist = self.alb_lists_new_2u[2][int(get_alb)]
-
-        print(u"Search for tracks from {0}.".format(unicode(get_alb_name)))
-        print(get_alb_tracklist)
 
         self.sngList.clear()
 
+        # get checked album title
+        get_alb_name = QtCore.QString(unicode(self.albList.currentItem().text()))
+        # ...and get his tracklist
+        get_alb_tracklist = self.alb_track_dict[unicode(get_alb_name)]
+
+        # console information
+        print(u"Search for tracks from {0}.".format(unicode(get_alb_name)))
+        print(get_alb_tracklist)
+
+        # open json file
         json_url = urllib.urlopen(get_alb_tracklist)
         data = json.load(json_url)
-
-        # -------------get-values-from-json-file----------------
-
-        # sng_id_list = []
-        sng_title_list = []
 
         # check index
         li = (len(data["data"]))
 
+        # get values from json file
+        sng_title_list = []
         for i in range(li):
-            # sng_id = data["data"][i]["id"]
-            # sng_id_list.append(sng_id)
-
             sng_title = data["data"][i]["title"]
             sng_title_list.append(sng_title)
 
-        # ---------remove-duplicates-from-list---------------
-
-        # sng_lists_3 = [sng_id_list] + [sng_title_list]
-        sng_lists_3 = [sng_title_list]
-        i3 = len(sng_lists_3) - 1
-        sng_lists_3u = [unique(sng_lists_3[i3]) for sng_lists_3[i3] in sng_lists_3[:]]
-
-        # --------------add-elements-to-list-----------------
-
-        [self.sngList.addItem(i) for i in sng_lists_3u[0]]
-
-    # -----------------play-album------------------------
+        # paste songs to list
+        [self.sngList.addItem(sng_title_list[i]) for i in range(len(sng_title_list))]
 
     def play_album(self):
 
         """
         Play chosen album.
-        Method check active current row in List Widget (albList), get his index (get_alb) and his name (get_alb_name).
-        Then using index (get_alb) get his id (get_alb_id) from list (alb_lists_2u). Supplements url (artist_player_url)
-        with album id (get_alb_id) and display playlist (class BottomPanel).
         """
-        get_alb = QtCore.QString(unicode(self.albList.currentRow()))
+        # get checked song title
         get_alb_name = QtCore.QString(unicode(self.albList.currentItem().text()))
+        # .. and get his tracklist
+        get_alb_id = self.alb_track_dict[unicode(get_alb_name)]
 
-        get_alb_id = self.alb_lists_new_2u[0][int(get_alb)]
+        # console information
         print(u"Play tracks from {0}.".format(unicode(get_alb_name)))
 
+        # find album id in tracklist
+        alb_id = re.search('(.*)album/(.*)/tracks(.*)', get_alb_id).group(2)
+
+        # open deezer widget
         artist_player_url = 'http://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&' \
                             'width=700&height=350&color=007FEB&layout=dark&size=medium&type=album&id={0}&' \
-                            'title=&app_id=1'.format(get_alb_id)
+                            'title=&app_id=1'.format(alb_id)
         self.classicWebView.setUrl(QtCore.QUrl(artist_player_url))
 
         self.show_bottom_panel()
-
 
 def unique(z):
     """
